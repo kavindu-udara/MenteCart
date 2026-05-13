e4d39a71ebbe489a9d68c3ea095405f680b67867
kavindu-udara <karunasinghesampath38@gmail.com>
2026-05-13 16:23:39 +0530

    configured express json

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,5 +1,17 @@
 import { NextFunction, Request, Response } from "express";
 
+type SignupRequest = {
+    username: string;
+    email: string;
+    password: string;
+};
+
 export const signup = async (req : Request, res : Response, next : NextFunction) => {
+    const { username, email, password } = req.body as SignupRequest;
+
+    if (!username || !email || !password) {
+        return res.status(400).json({ message: "All fields are required" });
+    }
+
   res.send("Signup endpoint");
 };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -7,6 +7,9 @@ dotenv.config();
 await connectDB();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const PORT = process.env.PORT || 3000;
 
 app.get('/test', (req, res) => {
